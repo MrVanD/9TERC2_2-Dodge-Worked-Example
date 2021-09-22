1e78d5a76025493664f04c270a68226d06f78597
E4116903 <kyle.vandordrecht@educaiton.wa.edu.au>
2021-09-22 08:16:49 +0800

    Created falling objects and gave them motion. Created player character without movement.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Basic Pygame Structure
 
 import pygame                               # Imports pygame and other libraries
+import random
 
 # Define Classes (sprites) here
 
@@ -8,6 +9,7 @@ pygame.init()                               # Pygame is initialised (starts runn
 
 screen = pygame.display.set_mode([700,500]) # Set the width and height of the screen [width,height]
 pygame.display.set_caption("Mr van's Dodge Game")         # Name your window
+background_image = pygame.image.load("OrchardBackground.jpg").convert()
 done = False                                # Loop until the user clicks the close button.
 clock = pygame.time.Clock()                 # Used to manage how fast the screen updates
 BLACK    = (   0,   0,   0)                 # Define some colors using rgb values.  These can be
@@ -18,6 +20,47 @@ BLUE     = (0, 0, 255)
 
 
 # Define additional Functions and Procedures here
+allFallingObjects = pygame.sprite.Group()
+
+class FallingObject(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.timeCreated = pygame.time.get_ticks()
+        self.image = pygame.Surface([30,30])
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0,670)
+        self.rect.y = 0
+
+    def setImage(self, graphicSelected):
+        fallingObjectImage = pygame.image.load(graphicSelected)
+        self.image.blit(fallingObjectImage,(0,0))
+
+    def moveFallingObjects(self, distance):
+        if self.rect.y <= 470:
+            self.rect.y = self.rect.y + distance
+
+    def deleteFallingObjects(self):
+        if self.rect.y > 470:
+            self.kill()
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([50,68])
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = 310
+        self.rect.y = 420
+
+        self.image.blit(pygame.image.load("Superhero.png"))
+
+nextApple = pygame.time.get_ticks() + 2500
+
+charactersGroup = pygame.sprite.Group()
+character = Character()
 
 # -------- Main Program Loop -----------
 while done == False:
@@ -27,7 +70,19 @@ while done == False:
             done = True                     # Flag that we are done so we exit this loop
 
     # Update sprites here
+    if pygame.time.get_ticks() > nextApple:
+        nextObject = FallingObject()
+        nextObject.setImage("Apple.png")
+        allFallingObjects.add(nextObject)
+        nextApple = pygame.time.get_ticks() + 1500
 
+    for eachObject in (allFallingObjects.sprites()):
+        eachObject.moveFallingObjects(5)
+
+        eachObject.deleteFallingObjects()
+
+    screen.blit(background_image, [0,0])
+    allFallingObjects.draw(screen)
     pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
     clock.tick(20)                          # Limit to 20 frames per second
 
